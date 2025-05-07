f8e46dfbce8b6d72507e585d952a544fb4677054
pycoe6 <pycoe6@gmail.com>
2025-05-07 23:09:01 +0200

    Add files via upload

---
+++ ckre.js
@@ -0,0 +1,32 @@
+let chatId = -1001223737350;
+
+function sendRequest() {
+  fetch('https://api.chatkeeper.app/cabinet/v1/tg/getchatconfig', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json, text/plain, */*',
+      'Origin': 'https://cabinet.chatkeeper.app',
+      'Referer': 'https://cabinet.chatkeeper.app/',
+    },
+    credentials: 'include',
+    body: JSON.stringify({ chat_id: chatId.toString() })
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(`Response for chat_id ${chatId}:`, data);
+  })
+  .catch(error => {
+    console.error(`Error for chat_id ${chatId}:`, error);
+  });
+
+  chatId--;
+  setTimeout(sendRequest, 5000);
+}
+
+sendRequest();
